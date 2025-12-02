1085ce889e80f757e5bcd9e83a306c1e1c640cde
pl4y3r123 <92274262+pl4y3r123@users.noreply.github.com>
2025-12-02 22:17:04 +0100

    Update LED_STRIPE.cpp

--- LED_STRIPE.cpp
+++ LED_STRIPE.cpp
@@ -1,21 +1,8 @@
-*/
+/*
 Stripe mit eigenem 24V Netzteil. 24+ an Stripe, GND an Stripe+Board GND, C(old) und W(arm) anschluss einfach mit digital Pin ansteuerbar um warm o kaltweiß zu schalten - hier in der Schaltung nicht berücksichtigt.
 PIN 6 an den DI des Stripes gehängt und mit Adafruit_Nepixel Library angesteuert.
 */
-
-#include <Adafruit_NeoPixel.h>
-
-#define PIN            6
-#define NUMPIXELS      100
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-void setup() {
-  pixels.begin();
-}
-
-void loop() {
-// Segmente 1-14, Helligkeit der Farben BLAU, ROT, GRUEN
+/* Segmente 1-14, Helligkeit der Farben ROT, GRUEN, BLAU
   pixels.setPixelColor(0, pixels.Color(0, 0, 100));
   pixels.setPixelColor(1, pixels.Color(0, 100, 0));
   pixels.setPixelColor(2, pixels.Color(100, 0, 0));
@@ -32,5 +19,214 @@ void loop() {
   pixels.setPixelColor(13, pixels.Color(100, 0, 0));
   pixels.setPixelColor(14, pixels.Color(100, 0, 0));
 // bei SHOW werden die Werte gesammelt an den LED Stripe geschickt.
-  pixels.show();
+  pixels.show(); */
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6
+#define NUMPIXELS      100
+
+const int bahnROTin = 2;  
+const int bahnGELBin = 3; 
+const int bahnGRUENin = 4;
+const int bahnBLAUin = 5; 
+const int bahnROTout = 7;  
+const int bahnGELBout = 8; 
+const int bahnGRUENout = 9;
+const int bahnBLAUout = 10; 
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  pixels.begin();
+  pinMode(bahnROTin, INPUT_PULLUP);
+  pinMode(bahnGELBin, INPUT_PULLUP);
+  pinMode(bahnBLAUin, INPUT_PULLUP);
+  pinMode(bahnGRUENin, INPUT_PULLUP);
+  pinMode(bahnROTout, OUTPUT);
+  pinMode(bahnGELBout, OUTPUT);
+  pinMode(bahnBLAUout, OUTPUT);
+  pinMode(bahnGRUENout, OUTPUT);
+
+
+}
+
+void loop() {
+
+digitalWrite(bahnROTout, HIGH);
+digitalWrite(bahnGELBout, HIGH);
+digitalWrite(bahnBLAUout, HIGH);
+digitalWrite(bahnGRUENout, HIGH);
+
+//ROT gewinnt
+if (digitalRead(bahnGRUENin) == LOW){
+digitalWrite(bahnROTout, LOW);
+digitalWrite(bahnGELBout, LOW);
+digitalWrite(bahnBLAUout, LOW);
+digitalWrite(bahnGRUENout, LOW);
+  for (int i = 0; i <= 20; i++) {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(1, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(3, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(4, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(5, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(6, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(7, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(8, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(10, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(11, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(12, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(13, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(14, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(200);
+pixels.setPixelColor(0, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(4, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(5, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(6, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(7, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(8, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(9, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(10, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(11, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(12, pixels.Color(0, 100, 0));
+  pixels.setPixelColor(13, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(14, pixels.Color(0, 100, 0));
+    pixels.show();
+    delay(200);
+    } 
+    }
+//GELB gewinnt
+if (digitalRead(bahnGELBin) == LOW){
+digitalWrite(bahnROTout, LOW);
+digitalWrite(bahnGELBout, LOW);
+digitalWrite(bahnBLAUout, LOW);
+digitalWrite(bahnGRUENout, LOW);
+  for (int i = 0; i <= 20; i++) {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(1, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(3, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(4, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(5, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(6, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(7, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(8, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(10, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(11, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(12, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(13, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(14, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(200);
+pixels.setPixelColor(0, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(4, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(5, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(6, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(7, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(8, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(9, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(10, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(11, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(12, pixels.Color(100, 100, 0));
+  pixels.setPixelColor(13, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(14, pixels.Color(100, 100, 0));
+    pixels.show();
+    delay(200);
+    } 
+    }
+if (digitalRead(bahnROTin) == LOW){
+digitalWrite(bahnROTout, LOW);
+digitalWrite(bahnGELBout, LOW);
+digitalWrite(bahnBLAUout, LOW);
+digitalWrite(bahnGRUENout, LOW);
+  for (int i = 0; i <= 20; i++) {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(1, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(3, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(4, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(5, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(6, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(7, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(8, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(10, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(11, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(12, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(13, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(14, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(200);
+pixels.setPixelColor(0, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(4, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(5, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(6, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(7, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(8, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(10, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(11, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(12, pixels.Color(100, 0, 0));
+  pixels.setPixelColor(13, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(14, pixels.Color(100, 0, 0));
+    pixels.show();
+    delay(200);
+    } 
+    }
+if (digitalRead(bahnBLAUin) == LOW){
+digitalWrite(bahnROTout, LOW);
+digitalWrite(bahnGELBout, LOW);
+digitalWrite(bahnBLAUout, LOW);
+digitalWrite(bahnGRUENout, LOW);
+  for (int i = 0; i <= 20; i++) {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(1, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(2, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(3, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(4, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(5, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(6, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(7, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(8, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(9, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(10, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(11, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(12, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(13, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(14, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(200);
+pixels.setPixelColor(0, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(2, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(3, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(4, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(5, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(6, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(7, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(8, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(9, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(10, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(11, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(12, pixels.Color(0, 0, 100));
+  pixels.setPixelColor(13, pixels.Color(0, 0, 0));
+  pixels.setPixelColor(14, pixels.Color(0, 0, 100));
+    pixels.show();
+    delay(200);
+    } 
+    }
+
 }
